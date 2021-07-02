5de802edeef3ca82e5c3c1d91c27a0b095c6da0c
OlyaLarchenko <55406108+OlyaLarchenko@users.noreply.github.com>
2021-07-02 22:27:11 +0300

    refactoring

--- max&minNumbers.js
+++ max&minNumbers.js
@@ -1,21 +1,19 @@
-let minNumber = Infinity;
-let maxNumber = -Infinity;
-function maxAndMinNumbers(array) {
+function minAndMax(array) {
+  let min = Infinity;
+let max = -Infinity;
   if (array instanceof Array) {
     for (let i = 0; i <= array.length; i++) {
-      if (maxNumber < array[i]) {
-        maxNumber = array[i];
+      if (max < array[i]) {
+        max = array[i];
+      }
+      if (min > array[i]) {
+        min = array[i];
       }
     }
-    for (let i = 0; i <= array.length; i++) {
-      if (minNumber > array[i]) {
-        minNumber = array[i];
-      }
-      console.log(`Maximum number is ${maxNumber}`);
-      console.log(`Minimum number is ${minNumber}`);
-    }
-  }
-  else {
-    console.log("It isn't array");
+    return {
+      min,
+      max,
+      };
   }
 }
+
