ce6ea8d023f3b3326b4dd35efbc1ee1c4a47261b
OlyaLarchenko <55406108+OlyaLarchenko@users.noreply.github.com>
2021-07-03 23:42:04 +0300

    implemented function which is checking if array is sorted

---
+++ checkIfSorted.js
@@ -0,0 +1,13 @@
+function checkIfSorted(array) {
+  if (array instanceof Array) {
+    for (let i = 0; i < array.length; i++) {
+      if (typeof array[i] !== "number") {
+        return;
+      }
+      if (array[i] > array[i + 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
