ad151aea78935d504c5921f3318e51cef1a74bae
OlyaLarchenko <55406108+OlyaLarchenko@users.noreply.github.com>
2021-07-04 19:20:56 +0300

    fix from code review

--- checkIfSorted.js
+++ checkIfSorted.js
@@ -1,13 +1,14 @@
 function checkIfSorted(array) {
-  if (array instanceof Array) {
-    for (let i = 0; i < array.length; i++) {
-      if (typeof array[i] !== "number") {
-        return;
-      }
-      if (array[i] > array[i + 1]) {
-        return false;
-      }
-    }
-    return true;
+  if (!(array instanceof Array)) {
+    return;
   }
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] !== "number") {
+      return;
+    }
+    if (array[i] > array[i + 1]) {
+      return false;
+    }
+  }
+  return true;
 }
